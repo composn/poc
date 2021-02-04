09701e890cd025fede703b9299dd15986d72f0bb
robin <robin.lebert@gmail.com>
2021-02-04 05:10:09 +0100

    feat: add jwt and dotenv

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import Koa from 'koa';
 import koaBody from 'koa-body';
+import jwt from 'koa-jwt';
+
 import autoLoadRouter from './auto-load-router';
 import permissionCheckController from './middlewares/permission-check-controller';
 
 const app = new Koa();
-const router = await autoLoadRouter({
-  middlewares: [permissionCheckController],
-});
+const router = await autoLoadRouter();
 
-app.use(koaBody()).use(router.routes()).use(router.allowedMethods());
+app
+  .use(jwt({ secret: process.env.JWT_SECRET, passthrough: true }))
+  .use(koaBody())
+  .use(permissionCheckController)
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
