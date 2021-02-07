cd0339c516a96ba574519c56972bbc83572623f1
robin <robin.lebert@gmail.com>
2021-02-07 08:29:45 +0100

    feat: add access control

--- index.js
+++ index.js
@@ -5,18 +5,58 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 import jwt from 'koa-jwt';
 
-import autoLoadRouter from './lib/auto-load-router';
-import permissionCheckController from './middlewares/permission-check-controller';
+import defineCtxRoles from './lib/middlewares/define-ctx-roles';
+import checkControllerPermission from './lib/middlewares/check-controller-permission';
+import AutoLoadRouter from './lib/auto-load-router';
+import ac from './lib/access-control';
+
+const rolesTree = {
+  name: 'public',
+  parents: [
+    {
+      name: 'authenticated',
+      parents: [
+        {
+          name: 'admin',
+        },
+      ],
+    },
+  ],
+};
+
+const grantsObject = {
+  public: {
+    grants: [{ action: 'foo', resource: 'bar' }],
+  },
+};
+
+ac.setGrants(grantsObject);
+ac.extendsFromTree(rolesTree);
 
 const app = new Koa();
-const router = await autoLoadRouter();
+
+const autoLoad = new AutoLoadRouter();
+await autoLoad.load();
+autoLoad.routes.forEach((route) => {
+  if (route.method === 'GET' && !route.grants) {
+    route.grants = [
+      {
+        role: 'public',
+        action: 'GET',
+        resource: route.joinPath,
+      },
+    ];
+  }
+  ac.addGrantsFromList(route.grants);
+});
+const router = autoLoad.getRouter(checkControllerPermission());
 
 app
   .use(
     jwt({ secret: process.env.JWT_SECRET, passthrough: true, key: 'payload' }),
   )
   .use(koaBody())
-  .use(permissionCheckController)
+  .use(defineCtxRoles())
   .use(router.routes())
   .use(router.allowedMethods());
 
